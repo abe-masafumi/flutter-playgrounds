17cf6ef1803ad06c7770e389bbba0193bc17304d
M1-Masa <masa455.wv@gmail.com>
2023-02-19 18:53:43 +0900

    app barのタブ切り替え

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -9,6 +9,7 @@ class LearnFlutterPage extends StatefulWidget {
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
   bool isSwitch = false;
+  bool isCheckbox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,74 +22,98 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back_ios),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('Acrtions Buttonが押されました');
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
       ),
-      body: Column(
-        children: [
-          Image.asset('images/pic001.jpeg'),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.blueGrey,
-            width: double.infinity,
-            child: const Center(
-              child: Text(
-                'This is a text widget',
-                style: TextStyle(
-                  color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/pic001.jpeg'),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'This is a text widget',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              debugPrint('Elevated Buttonが押されました');
-            },
-            child: const Text('Elevated Button'),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint('Outlined Buttonが押されました');
-            },
-            child: const Text('Outlined Button'),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint('Text Buttonが押されました');
-            },
-            child: const Text('Text Button'),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint('このブロックがTapされました');
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.blue,
-                ),
-                Text('Row widget'),
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.blue,
-                ),
-              ],
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint('Elevated Buttonが押されました');
+              },
+              child: const Text('Elevated Button'),
             ),
-          ),
-          Switch(value: isSwitch, onChanged: (bool newBool){
-            setState(() {
-              isSwitch = newBool;
-            });
-          })
-        ],
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('Outlined Buttonが押されました');
+              },
+              child: const Text('Outlined Button'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('Text Buttonが押されました');
+              },
+              child: const Text('Text Button'),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('このブロックがTapされました');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text('Row widget'),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckbox,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isCheckbox = newBool;
+                  });
+                }),
+            Image.network(
+                'https://media.loom-app.com/bi/dist/images/2019/11/13/shutterstock_1306747210-1.jpg?w=640'),
+          ],
+        ),
       ),
     );
   }
